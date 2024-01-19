852b2a77934c82617d173e7eb50ad4cd14305233
Azimbek <omar.azimbek@gmail.com>
2024-01-19 22:16:52 +0600

    1.4.3 Commit

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,59 @@
 package com.epam.mjc.collections.map;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class KeyValueSwapper {
-    public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+    public static Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> map = new HashMap<>();
+
+        if (sourceMap.isEmpty()) {
+        } else {
+            List<Integer> listKeys = new ArrayList<>();
+            List<String> listValues = new ArrayList<>();
+
+            // save all the values and keys of the sourceMap into the lists
+            for (Map.Entry<Integer, String> e : sourceMap.entrySet()) {
+                listKeys.add(e.getKey());
+                listValues.add(e.getValue());
+            }
+
+            // find out a bigger key of the same values and remove it from the sourceMap
+            Integer smallKey = null;
+            String sameValue = null;
+            for (int i=0; i<listValues.size()-1; i++) {
+                for (int j=i+1; j<listValues.size(); j++) {
+                    if (listValues.get(i).equals(listValues.get(j))) {
+                        sameValue = listValues.get(i);
+                        if (listKeys.get(i)<listKeys.get(j)) {
+                            smallKey = listKeys.get(i);
+                        } else {
+                            smallKey = listKeys.get(j);
+                        }
+                    }
+                }
+            }
+
+            // finally just swap the key and values into the map
+            for (Map.Entry<Integer, String> e : sourceMap.entrySet()) {
+                if (e.getValue().equals(sameValue)) {
+                    map.put(e.getValue(), smallKey);
+                } else {
+                    map.put(e.getValue(), e.getKey());
+                }
+            }
+        }
+
+        return map;
+    }
+    public static void main(String[] args) {
+        Map<Integer, String> sourceMap = new HashMap<>();
+        sourceMap.put(10, "one");
+        sourceMap.put(2, "two");
+        sourceMap.put(3, "three");
+        sourceMap.put(1, "one");
+        System.out.println(swap(sourceMap));
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,41 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
-    public Map<String, Integer> createWordRepetitionMap(String sentence) {
+    public static Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> map = new HashMap<>();
+
+        if (sentence.isEmpty()) {
+        } else {
+            String[] words = sentence.split("\\W+");
+            int counter = 1;
+
+            for (int i=0; i<words.length-1; i++) {
+                words[i] = words[i].toLowerCase();
+                for (int j=i+1; j<words.length; j++) {
+                    words[j] = words[j].toLowerCase();
+                    if (words[i].equals(words[j])) {
+                        counter++;
+                    }
+                }
+                if (!map.containsKey(words[i])) {
+                    map.put(words[i], counter);
+                }
+                counter=1;
+            }
+
+            if (!map.containsKey(words[words.length-1])) {
+                map.put(words[words.length-1], counter);
+            }
+        }
+
+        return map;
+    }
+    public static void main(String[] args) {
+        String s1 = "hello word in lOwEr cAsE, HELLO WORD in UpPeR CaSe.";
+        String s2 = "Java Map can store pairs of keys and values.";
+        System.out.println(createWordRepetitionMap(s2));
     }
 }
